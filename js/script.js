919af1394b6299cdc358b526092a8ad80ced3dce
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2021-12-07 23:19:13 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,83 +1,84 @@
-// every(); все елементы массива больше данного числа true/false
+// // every(); все елементы массива больше данного числа true/false
 console.log('every()');
-const arr6 = [1,10,15];
-const arrDouble6 = [10, 12, 50];
-function everyCheck(currentValue, array) {
+const arrayEvery1 = [11, 9, 12, 13];
+const arrayEvery2 = [11, 10, 12, 13];
+function every(array, callback) {
     for (let i = 0; i < array.length; i++){
-        if (array[i] >= currentValue) {
-            return true;
-        }
-        return false;
+        if (!callback(array[i], i, array)) return false;
     }
+    return true;
 }
-console.log(everyCheck(10, arr6));//false
-console.log(everyCheck(10, arrDouble6));//true
+console.log(every(arrayEvery1, (item, i, array) => item >= 10));//false
+console.log(every(arrayEvery2, (item, i, array) =>  item >= 10));//true
 
 //some() яв. ли какой-либо элемент массива четным true/false
 console.log('some()');
-const arr7 = [1,3,6,5,3,7];
-const arrDouble7 = [1, 11, 1, 13, 5, 11];
-function someElement(array) {
-    for (let i = 0; i <= array.length; i++){
-        if (array[i] % 2 === 0) {
-            return true;
-        }
+const arraySome1 = [11, 9, 11, 13];
+const arraySome2 = [11, 10, 8, 15];
+function some(array, callback) {
+    for (let i = 0; i < array.length; i++){
+        if (callback(array[i], i, array)) return true;
     }
     return false;
 }
-console.log(someElement(arr7));//true
-console.log(someElement(arrDouble7));//false
+console.log(some(arraySome1, (item, i, array) => item % 2 === 0));//false
+console.log(some(arraySome2, (item, i, array) =>  item % 2 === 0));//true
 
 //lastIndexOf поиск елемента по массиву с конца
 console.log('lastIndexOf()');
 const arr = ['djess','dasha', 'olya', 'vova','dasha'];
 function lastIndexOf(arr, item, fromIndex = null) {
-    fromIndex === null ? fromIndex = arr.length -1 : null;
+    if(fromIndex === null) fromIndex = arr.length -1;
     for (let i = fromIndex; i >= 0; i--){
         if (arr[i] === item) return i;
     }
     return -1;
-    }
-console.log(lastIndexOf(arr, 'dasha'));
+}
+console.log(lastIndexOf(arr, 'dasha'));//4
 
 //IndexOf поиск елемента по массиву сначала
 console.log('IndexOf()');
 const arr2 = [1, 2, 3, 4, 3, 1];
-function IndexOf(arr, item) {
-        for (let i = 0; i <= arr.length; i++){
-            if (arr[i] === item) return i;
-        }
+function IndexOf(arr, item, fromIndex = null) {
+    if (fromIndex >= arr.length - 1) {
         return -1;
+    } 
+    for (let i = 0; i <= arr.length; i++){
+        if (arr[i] === item) return i;
     }
-console.log(IndexOf(arr2, 1));
+    return -1;
+    }
+console.log(IndexOf(arr2, 4));//3
 
 //find()
 console.log('find()');
-const arr3 = [1, 22, 3, 4];
+const arr3 = [1, 26, 3, 4];
 const arrDouble3 = [1, 3, 1, 7, 5, 11];
+
 function findElement(array) {
     for (let i = 0; i <= array.length; i++){
-        if (array[i] % 2 === 0) return array[i];
-        
+        if (evenNumbers(array[i], i, array)) return array[i];
     }
     return undefined;
 }
-console.log(findElement(arr3));//22
-console.log(findElement(arrDouble3));//undefined
+console.log(findElement(arr3,evenNumbers));//22
+console.log(findElement(arrDouble3,evenNumbers));//undefined
 
 //findIndex поиск четного числа в массиве вывод индекса
-// console.log('findIndex()');
+console.log('findIndex()');
 const arr4 = [1, 21, 24, 4];
 const arrDouble4 = [1, 3, 1, 7, 5, 11];
+function evenNumbers(element,index,array) {
+    return element % 2 === 0;
+}
 function findIndex(array) {
     for (let i = 0; i <= array.length; i++){
-        if (array[i] % 2 === 0) return i;
-        
+        if (evenNumbers(array[i], i, array)) return i;
     }
     return -1;
 }
-console.log(findIndex(arr4));//2
-console.log(findIndex(arrDouble4));//-1
+console.log(findIndex(arr4,evenNumbers));//2
+console.log(findIndex(arrDouble4,evenNumbers));//-1
 
 
 //includes
